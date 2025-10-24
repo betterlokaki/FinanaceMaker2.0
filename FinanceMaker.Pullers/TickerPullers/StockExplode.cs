d0cce9928756a7600555280b579cc3552b628bb4
LoKaki2.0 <BetterLoKaki@gmail.com>
2025-10-24 17:01:00 +0300

    aaaa

--- FinanceMaker.Pullers/TickerPullers/StockExplode.cs
+++ FinanceMaker.Pullers/TickerPullers/StockExplode.cs
@@ -56,10 +56,10 @@ public class StockExplode : FinvizTickersPuller
 
                         var priceRisk = 0.05; // 4% range
 
-                        if (weeklyCandles.Last().IsRed && latestPrice <= gapStartPrice * (1 + priceRisk) && latestPrice >= gapStartPrice * (1 - priceRisk))
+                        // if (weeklyCandles.Last().IsRed && latestPrice <= gapStartPrice * (1 + priceRisk) && latestPrice >= gapStartPrice * (1 - priceRisk))
                         {
                             relevantTickers.Add(ticker);
-                            break;
+                            // break;
 
                         }
                     }
